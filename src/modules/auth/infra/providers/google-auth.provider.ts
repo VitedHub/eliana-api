d766ff63eadb0b9b7471364d5de6492f79dfd561
GeyzonErik <Geyzon100@gmail.com>
2025-03-06 23:21:06 -0300

    feature: create google auth provider

---
+++ src/modules/auth/infra/providers/google-auth.provider.ts
@@ -0,0 +1,63 @@
+import {
+  ExchangeCodeForTokensOutput,
+  GetUserDataOutput,
+  IAuthProvider,
+} from '@/auth/application/providers/auth.provider';
+import { AUTH_PROVIDER } from '@/clients/domain/enums/auth-provider.enum';
+import { IHttpClient } from '@/core/application/providers/http-client.provider';
+import { Inject } from '@nestjs/common';
+
+export type GoogleTokenResponse = {
+  access_token: string;
+  refresh_token: string;
+  expires_in: number;
+  scope: string;
+  token_type: string;
+};
+
+export class GoogleAuthProvider implements IAuthProvider {
+  @Inject(IHttpClient)
+  private readonly httpClient: IHttpClient;
+
+  async exchangeCodeForTokens(
+    code: string,
+  ): Promise<ExchangeCodeForTokensOutput> {
+    const url = 'https://oauth2.googleapis.com/token';
+
+    const response = await this.httpClient.post<GoogleTokenResponse>(url, {
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      code,
+      redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+      grant_type: 'authorization_code',
+    });
+
+    if (!response.data.access_token || !response.data.refresh_token) {
+      throw new Error(response.data.error || 'Code change for token error');
+    }
+
+    return {
+      accessToken: response.data.access_token,
+      refreshToken: response.data.refresh_token,
+    };
+  }
+
+  async getUserData(accessToken: string): Promise<GetUserDataOutput> {
+    const url = 'https://www.googleapis.com/oauth2/v3/userinfo';
+
+    const response = await this.httpClient.get<{
+      sub: string;
+      email: string;
+      name: string;
+    }>(url, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    return {
+      name: response.data.name,
+      email: response.data.email,
+      authProvider: AUTH_PROVIDER.GOOGLE,
+      oAuthId: response.data.sub,
+    };
+  }
+}
